b847f95db6e1153104393b61814e55e6fbb81790
AlexCleberLimaFerreira <alexcleberlimaferreira@gmail.com>
2024-05-28 20:21:53 -0300

    Primeiro desafio

---
+++ desafio1.js
@@ -0,0 +1,19 @@
+// Faça duas variáveis numéricas e descubra qual é a maior
+
+let a = 5
+let b = 10
+
+if(a> b){
+console.log('A maior que B')
+}
+
+
+
+else if(a < b ) {
+    console.log('B é maior que A')
+}
+
+
+else{
+    console.log('São iguais')
+}
